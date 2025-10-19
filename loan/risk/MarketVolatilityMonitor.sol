8424d280b53a4b05895701a4138ea7068ba2ea29
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:24:57 +0800

    Update MarketVolatilityMonitor.sol

--- loan/risk/MarketVolatilityMonitor.sol
+++ loan/risk/MarketVolatilityMonitor.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IMarketVolatilityMonitor {
-    // 市場波動監控
     function getVolatility(address _asset) external view returns (uint256);
     function setVolatilityThreshold(uint256 _threshold) external;
     function isVolatile(address _asset) external view returns (bool);
