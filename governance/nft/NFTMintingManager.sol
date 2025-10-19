d48b5f2a0e3899f4d696ca0aa47ce9caea142577
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:40:29 +0800

    Update NFTMintingManager.sol

--- governance/nft/NFTMintingManager.sol
+++ governance/nft/NFTMintingManager.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface INFTMintingManager {
-    // 鑄造管理
     function mintNFT(address _to, uint256 _tokenId) external;
     function setMintingEnabled(bool _enabled) external;
     function isMintingEnabled() external view returns (bool);
