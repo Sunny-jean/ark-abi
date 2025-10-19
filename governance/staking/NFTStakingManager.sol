3a2326faaeebc13858c39bb7432f7f66eeeb350a
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:45:43 +0800

    Update NFTStakingManager.sol

--- governance/staking/NFTStakingManager.sol
+++ governance/staking/NFTStakingManager.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface INFTStakingManager {
-    // NFT 質押控制器
     function stakeNFT(uint256 _tokenId) external;
     function unstakeNFT(uint256 _tokenId) external;
     function getStakedNFTs(address _user) external view returns (uint256[] memory);
