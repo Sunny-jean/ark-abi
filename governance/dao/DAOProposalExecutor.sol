aa817f6c16bf85283750814ac413eb6f1762686d
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:26:45 +0800

    Update DAOProposalExecutor.sol

--- governance/dao/DAOProposalExecutor.sol
+++ governance/dao/DAOProposalExecutor.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IDAOProposalExecutor {
-    // DAO 提案執行
     function executeDAOProposal(uint256 _proposalId) external;
     function setExecutorRole(address _executor, bool _enabled) external;
     function getExecutorRole(address _executor) external view returns (bool);
