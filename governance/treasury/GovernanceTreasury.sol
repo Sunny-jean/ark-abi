d119caa1860b436414dff0cc321d482ef80c9594
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:51:46 +0800

    Update GovernanceTreasury.sol

--- governance/treasury/GovernanceTreasury.sol
+++ governance/treasury/GovernanceTreasury.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IGovernanceTreasury {
-    // 治理金庫
     function deposit(address _token, uint256 _amount) external;
     function withdraw(address _token, uint256 _amount) external;
     function getBalance(address _token) external view returns (uint256);
