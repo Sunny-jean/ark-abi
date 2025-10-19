253759eb7928822e79388c59304b653cfe3862ad
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:22:32 +0800

    Update PriceFeedValidator.sol

--- loan/liquidation/PriceFeedValidator.sol
+++ loan/liquidation/PriceFeedValidator.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IPriceFeedValidator {
-    // 清算價格來源驗證
     function validatePrice(address _asset, uint256 _price) external view returns (bool);
     function setPriceFeed(address _asset, address _feedAddress) external;
     function getPriceFeed(address _asset) external view returns (address);
