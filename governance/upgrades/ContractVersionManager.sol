e7e40ae135401b11ee36dc060662fe1350ac72cd
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:52:10 +0800

    Update ContractVersionManager.sol

--- governance/upgrades/ContractVersionManager.sol
+++ governance/upgrades/ContractVersionManager.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IContractVersionManager {
-    // 合約版本管理
     function setContractVersion(address _contractAddress, uint256 _version) external;
     function getContractVersion(address _contractAddress) external view returns (uint256);
 
