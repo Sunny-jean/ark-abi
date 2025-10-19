8478d03a7d7dbff6670b3ec6098a1bf6811eeff2
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:20:23 +0800

    Update InterestRateModel.sol

--- loan/interest/InterestRateModel.sol
+++ loan/interest/InterestRateModel.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IInterestRateModel {
-    // 基礎利率模型（複利 / 線性）
     function getBorrowRate(address _asset, uint256 _utilization) external view returns (uint256);
     function getSupplyRate(address _asset, uint256 _utilization) external view returns (uint256);
     function setBaseRate(address _asset, uint256 _rate) external;
