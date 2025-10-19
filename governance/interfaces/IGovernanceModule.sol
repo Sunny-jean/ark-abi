5ca5d5a326b0f9c936d9001ed30607bf3474a883
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 14:42:57 +0800

    Update IGovernanceModule.sol

--- governance/interfaces/IGovernanceModule.sol
+++ governance/interfaces/IGovernanceModule.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IGovernanceModule {
-    // 治理模組通用介面
     function initialize() external;
     function shutdown() external;
     function getStatus() external view returns (bool);
