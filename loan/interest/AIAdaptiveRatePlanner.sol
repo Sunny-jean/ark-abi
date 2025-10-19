5943f357ddf511dbdb243e493f3e2c5398da4c11
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:19:53 +0800

    Update AIAdaptiveRatePlanner.sol

--- loan/interest/AIAdaptiveRatePlanner.sol
+++ loan/interest/AIAdaptiveRatePlanner.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IAIAdaptiveRatePlanner {
-    // AI 驅動利率預測與設置
     function predictAndSetRates() external;
     function getPredictedBorrowRate(address _asset) external view returns (uint256);
     function getPredictedSupplyRate(address _asset) external view returns (uint256);
