6886e33b306e75e6f7db64af5fc0632b84533eab
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:40:01 +0800

    Update NFTLifecycleController.sol

--- governance/nft/NFTLifecycleController.sol
+++ governance/nft/NFTLifecycleController.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface INFTLifecycleController {
-    // NFT 生命週期控制
     function pauseNFT(uint256 _tokenId) external;
     function unpauseNFT(uint256 _tokenId) external;
     function isPaused(uint256 _tokenId) external view returns (bool);
