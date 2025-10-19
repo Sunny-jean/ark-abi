538c84c0a66ca4c4a0e16536e4a41b6d5c94b09c
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:41:28 +0800

    Update ParameterValidator.sol

--- governance/parameters/ParameterValidator.sol
+++ governance/parameters/ParameterValidator.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IParameterValidator {
-    // 參數驗證
     function validateParameter(string calldata _parameterName, bytes calldata _value) external view returns (bool);
     function setValidationRule(string calldata _parameterName, address _validatorAddress) external;
 
