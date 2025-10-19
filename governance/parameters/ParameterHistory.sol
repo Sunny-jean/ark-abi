84da30cbdfa06588ef3823210a49e4d4724e3590
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:41:15 +0800

    Update ParameterHistory.sol

--- governance/parameters/ParameterHistory.sol
+++ governance/parameters/ParameterHistory.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IParameterHistory {
-    // 參數歷史記錄
     function getParameterHistory(string calldata _parameterName) external view returns (bytes[] memory values, uint256[] memory timestamps);
     function recordParameterChange(string calldata _parameterName, bytes calldata _newValue) external;
 
