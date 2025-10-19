dde609ee82826acd8e65e31bc278c6fe027c8413
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:21:59 +0800

    Update LiquidationIncentiveDistributor.sol

--- loan/liquidation/LiquidationIncentiveDistributor.sol
+++ loan/liquidation/LiquidationIncentiveDistributor.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface ILiquidationIncentiveDistributor {
-    // 清算獎勵分配器
     function distributeIncentive(address _liquidator, address _asset, uint256 _amount) external;
     function setIncentiveRate(uint256 _rate) external;
     function getIncentiveRate() external view returns (uint256);
