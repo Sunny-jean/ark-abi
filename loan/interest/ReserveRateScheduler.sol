4981ea76cbaaa1069b704004fd8c461e063c065d
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:20:39 +0800

    Update ReserveRateScheduler.sol

--- loan/interest/ReserveRateScheduler.sol
+++ loan/interest/ReserveRateScheduler.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IReserveRateScheduler {
-    // 協議準備金比例調整器
     function scheduleReserveAdjustment(uint256 _newRatio) external;
     function getScheduledRatio() external view returns (uint256);
     function executeAdjustment() external;
