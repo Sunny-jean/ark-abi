6df5591790cbf429a14d920c5efea7c531758f76
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:18:41 +0800

    Update CollateralManager.sol

--- loan/core/CollateralManager.sol
+++ loan/core/CollateralManager.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface ICollateralManager {
-    // 抵押品存入與管理
     function deposit(address _collateralAsset, uint256 _amount) external;
     function withdraw(address _collateralAsset, uint256 _amount) external;
     function getCollateralValue(address _user, address _collateralAsset) external view returns (uint256);
