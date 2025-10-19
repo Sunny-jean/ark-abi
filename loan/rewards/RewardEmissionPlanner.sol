088e31bb3ea153e505d85dea86e546c745166d96
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:23:46 +0800

    Update RewardEmissionPlanner.sol

--- loan/rewards/RewardEmissionPlanner.sol
+++ loan/rewards/RewardEmissionPlanner.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IRewardEmissionPlanner {
-    // LP 激勵發放排程
     function scheduleEmission(uint256 _startTime, uint256 _endTime, uint256 _amount) external;
     function getScheduledEmission(uint256 _scheduleId) external view returns (uint256 startTime, uint256 endTime, uint256 amount);
     function cancelEmission(uint256 _scheduleId) external;
