a8b3ffb0e3b220df2b7a2e7d646802d9f0751a7f
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:24:16 +0800

    Update LendingHealthChecker.sol

--- loan/risk/LendingHealthChecker.sol
+++ loan/risk/LendingHealthChecker.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface ILendingHealthChecker {
-    // 每筆倉位健康度追蹤
     function getPositionHealth(address _user) external view returns (uint256);
     function checkPositionHealth(address _user) external view returns (bool);
     function setHealthFactorThreshold(uint256 _threshold) external;
