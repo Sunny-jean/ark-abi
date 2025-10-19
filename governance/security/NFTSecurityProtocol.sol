26864ce1a0cc6113e004115e72cd463dfbc4562a
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:43:18 +0800

    Update NFTSecurityProtocol.sol

--- governance/security/NFTSecurityProtocol.sol
+++ governance/security/NFTSecurityProtocol.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface INFTSecurityProtocol {
-    // 安全規範合約
     function enforceSecurityPolicy(uint256 _tokenId) external;
     function setSecurityPolicy(address _policy) external;
 
