3abf2ce9bdf357ddd66a3fbc5ad8a1d28b738665
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:27:40 +0800

    Update NFTPermissionValidator.sol

--- governance/access/NFTPermissionValidator.sol
+++ governance/access/NFTPermissionValidator.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface INFTPermissionValidator {
-    // 權限驗證器
     function validatePermission(address _user, uint256 _permissionId) external view returns (bool);
     function setPermission(uint256 _permissionId, bool _enabled) external;
     function getPermissionStatus(uint256 _permissionId) external view returns (bool);
