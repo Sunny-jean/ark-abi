d45c587760ead838edd448170b48a91c06d20802
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:29:41 +0800

    Update CrossChainVoteAggregator.sol

--- governance/crosschain/CrossChainVoteAggregator.sol
+++ governance/crosschain/CrossChainVoteAggregator.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface ICrossChainVoteAggregator {
-    // 跨鏈投票聚合
     function aggregateVotes(uint256 _proposalId, uint256 _sourceChainId, uint256 _votesFor, uint256 _votesAgainst) external;
     function getAggregatedVotes(uint256 _proposalId) external view returns (uint256 totalVotesFor, uint256 totalVotesAgainst);
 
