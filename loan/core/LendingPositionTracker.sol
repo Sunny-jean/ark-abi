42ea4f88d64f936df2f6477f3a07ccac3fbf6d7f
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:19:15 +0800

    Update LendingPositionTracker.sol

--- loan/core/LendingPositionTracker.sol
+++ loan/core/LendingPositionTracker.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface ILendingPositionTracker {
-    // 借貸倉位記錄與使用者狀態
     function getUserBorrowBalance(address _user, address _asset) external view returns (uint256);
     function getUserCollateralBalance(address _user, address _collateralAsset) external view returns (uint256);
     function updateBorrowBalance(address _user, address _asset, uint256 _newBalance) external;
