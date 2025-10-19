d5908db0bb339a1ca270695d832e2099e6d55aa6
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:29:26 +0800

    Update CrossChainProposalRelayer.sol

--- governance/crosschain/CrossChainProposalRelayer.sol
+++ governance/crosschain/CrossChainProposalRelayer.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface ICrossChainProposalRelayer {
-    // 跨鏈提案中繼
     function relayProposal(uint256 _proposalId, uint256 _targetChainId) external;
     function setRelayerAddress(address _relayer) external;
     function getRelayerAddress() external view returns (address);
