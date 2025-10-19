dd888e49a66ca9b63107d3d9c13070f700d2d686
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:24:43 +0800

    Update LiquidityStressTester.sol

--- loan/risk/LiquidityStressTester.sol
+++ loan/risk/LiquidityStressTester.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface ILiquidityStressTester {
-    // 資金流動性壓力測試模組
     function runStressTest() external;
     function getStressTestResult() external view returns (bool);
     function setLiquidityThreshold(uint256 _threshold) external;
