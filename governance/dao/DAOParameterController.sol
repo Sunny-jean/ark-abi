d50f967011d81b8946c00f11687cc1391eb3636f
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:26:33 +0800

    Update DAOParameterController.sol

--- governance/dao/DAOParameterController.sol
+++ governance/dao/DAOParameterController.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IDAOParameterController {
-    // DAO 參數控制
     function setDAOParameter(string calldata _parameterName, bytes calldata _value) external;
     function getDAOParameter(string calldata _parameterName) external view returns (bytes memory);
 
