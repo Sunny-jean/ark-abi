2f239e617b7823cfeb8ccc02b28253d5d09ec61b
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:24:29 +0800

    Update LendingSecurityPolicy.sol

--- loan/risk/LendingSecurityPolicy.sol
+++ loan/risk/LendingSecurityPolicy.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface ILendingSecurityPolicy {
-    // 借貸風險總體策略
     function enforcePolicy(address _user, address _asset, uint256 _amount) external view returns (bool);
     function setPolicyParameter(uint256 _paramId, uint256 _value) external;
     function getPolicyParameter(uint256 _paramId) external view returns (uint256);
