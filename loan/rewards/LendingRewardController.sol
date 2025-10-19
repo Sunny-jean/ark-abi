6326a948dd9fbce2ceec388602d4eb88a133bcdc
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:23:28 +0800

    Update LendingRewardController.sol

--- loan/rewards/LendingRewardController.sol
+++ loan/rewards/LendingRewardController.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface ILendingRewardController {
-    // 借貸雙邊激勵（借出/借入）
     function distributeBorrowRewards(address _user, uint256 _amount) external;
     function distributeSupplyRewards(address _user, uint256 _amount) external;
     function setBorrowRewardRate(uint256 _rate) external;
