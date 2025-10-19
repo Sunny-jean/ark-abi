b043ecd90d1bd162df807c2413063103b0d796ed
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:31:54 +0800

    Update NFTTradeValidator.sol

--- governance/marketplace/NFTTradeValidator.sol
+++ governance/marketplace/NFTTradeValidator.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface INFTTradeValidator {
-    // 安全交易驗證
     function validateTrade(uint256 _tokenId, address _buyer, uint256 _price) external view returns (bool);
     function setValidationRule(uint256 _ruleId, bool _enabled) external;
 
