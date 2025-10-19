6b24659a88b9126941f9f0554537a08a2f943bf9
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:51:28 +0800

    Update FundAllocationManager.sol

--- governance/treasury/FundAllocationManager.sol
+++ governance/treasury/FundAllocationManager.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IFundAllocationManager {
-    // 資金分配管理
     function allocateFunds(address _to, address _token, uint256 _amount) external;
     function setAllocationStrategy(address _strategy) external;
     function getAllocationStrategy() external view returns (address);
