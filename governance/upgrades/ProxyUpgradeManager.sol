4586b64aa1e12cf5b90884b97541f27f26fe7361
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:55:06 +0800

    Update ProxyUpgradeManager.sol
    Removed comment in IProxyUpgradeManager interface.

--- governance/upgrades/ProxyUpgradeManager.sol
+++ governance/upgrades/ProxyUpgradeManager.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IProxyUpgradeManager {
-    // 代理合約升級
     function upgradeProxy(address _proxyAddress, address _newImplementation) external;
     function setUpgradeabilityStatus(address _proxyAddress, bool _upgradable) external;
     function getUpgradeabilityStatus(address _proxyAddress) external view returns (bool);
