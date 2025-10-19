2760bf333e1c39cc22f4e3e071682edc9659c24b
minty-conjurer <minty.wave01@ark.pro>
2025-10-19 09:20:09 +0800

    Update DynamicRateAdjuster.sol

--- loan/interest/DynamicRateAdjuster.sol
+++ loan/interest/DynamicRateAdjuster.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IDynamicRateAdjuster {
-    // 動態利率調整器（市場主導）
     function adjustRates() external;
     function setAdjustmentFactor(uint256 _factor) external;
     function getAdjustmentFactor() external view returns (uint256);
